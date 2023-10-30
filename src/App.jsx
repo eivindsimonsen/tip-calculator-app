043886b6bd88fd684b96eb9420be5ea25153c381
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-10-30 19:45:57 +0100

    Code splitted to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import "./css/index.css";
+import logo from "./assets/images/logo.svg";
+import Bill from "./components/Bill";
+import Tip from "./components/Tip";
+import People from "./components/People";
 
 function App() {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div className="wrapper">
+      <header>
+        <img
+          src={logo}
+          alt="Splitter"
+          className="logo"
+        />
+      </header>
+      <main>
+        <section className="calculation">
+          <Bill />
+          <Tip />
+          <People />
+        </section>
+        <section className="result">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, harum labore quia a quis cumque tempora aliquam praesentium, odio voluptate veritatis ipsum nemo totam? Temporibus cum aperiam harum laboriosam quos ad debitis ut deserunt sed quibusdam. Ea iure fugit laboriosam vel, consequuntur a, quasi eius doloremque, facilis ad aliquid sapiente!</p>
+        </section>
+      </main>
+    </div>
   );
 }
 
