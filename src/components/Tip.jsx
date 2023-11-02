f19042f4f872ee941ce4375d5793657c35bb68b4
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-11-02 22:09:42 +0100

    custom button functionality fixed

--- src/components/Tip.jsx
+++ src/components/Tip.jsx
@@ -2,36 +2,65 @@ import { useState } from "react";
 
 function Tip() {
   const [tipValue, setTipValue] = useState(0);
+  const [toggleCustom, setToggleCustom] = useState(false);
 
   const getValue = (value) => {
     setTipValue(value);
   };
 
+  const getCustomValue = (e) => {
+    const numericValue = parseInt(e.target.value, 10);
+    setTipValue(numericValue);
+
+    if (setTipValue > 0) {
+      setTipValue(0);
+    }
+  };
+
+  const toggleCustomValue = () => {
+    setToggleCustom(!toggleCustom);
+    setTipValue(0);
+  };
+
   return (
     <div className="input-container">
-      <label htmlFor="tip">Select Tip %</label>
+      <label>Select Tip %</label>
       <div id="tip">
         {[5, 10, 15, 25, 50].map((value) => (
           <input
             key={value}
             type="button"
-            value={`${value}%`}
-            onClick={() => getValue(value)}
+            aria-label={`Select ${value}%`}
+            autoComplete="off"
             className={tipValue === value ? "active" : ""}
+            value={`${value}%`}
+            onClick={() => {
+              getValue(value);
+              setToggleCustom(false);
+            }}
           />
         ))}
 
-        <input
-          type="button"
-          value="Custom"
-          id="tip-custom"
-          onClick={() => {
-            const customValue = parseFloat(prompt("Enter a custom tip percentage:"));
-            if (!isNaN(customValue)) {
-              getValue(customValue);
-            }
-          }}
-        />
+        {toggleCustom ? (
+          <input
+            type="text"
+            placeholder="0"
+            onChange={getCustomValue}
+            className="custom-value-input custom-value-input--selected"
+            aria-label="Custom Tip Percentage"
+            autoComplete="off"
+          />
+        ) : (
+          <input
+            type="text"
+            placeholder="Custom"
+            id="tip-custom"
+            onClick={toggleCustomValue}
+            className="custom-value-input"
+            aria-label="Custom Tip Percentage"
+            autoComplete="off"
+          />
+        )}
       </div>
     </div>
   );
