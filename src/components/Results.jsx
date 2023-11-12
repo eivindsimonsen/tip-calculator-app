e05088de3a97c556ed8c9f7f91b678388d82fe94
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-11-12 21:21:23 +0100

    Calculate values and display on screen

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,35 @@
 import { useCalculateContext } from "../context/CalculationContext";
 
 function Results() {
-  const { bill, tipValue, people } = useCalculateContext();
+  const { bill, setBill, tipValue, setTipValue, people, setPeople } = useCalculateContext();
 
-  console.log(bill, tipValue, people);
+  function calculateTipAmount(bill, tipPercentage, people) {
+    if (!bill || !tipPercentage || !people || people === 0) {
+      return "0.00";
+    }
+
+    const tipAmount = (bill * (tipPercentage / 100)) / people;
+    return tipAmount.toFixed(2);
+  }
+
+  function calculateTotalPrice(bill, tipPercentage, people) {
+    if (!bill || !tipPercentage || !people || people === 0) {
+      return "0.00";
+    }
+
+    const tipAmount = (bill * (tipPercentage / 100)) / people;
+    const totalAmount = bill / people + parseFloat(tipAmount);
+    return totalAmount.toFixed(2);
+  }
+
+  const tipAmountPerPerson = calculateTipAmount(bill, tipValue, people);
+  const totalAmountPerPerson = calculateTotalPrice(bill, tipValue, people);
+
+  const handleReset = () => {
+    setBill(0);
+    setTipValue(0);
+    setPeople(0);
+  };
 
   return (
     <>
@@ -12,17 +38,22 @@ function Results() {
           <p>Tip Amount</p>
           <span className="text-span">/ person</span>
         </div>
-        <span className="number-span">$4.27</span>
+        <span className="number-span">${tipAmountPerPerson}</span>
       </div>
       <div className="result-total">
         <div>
           <p>Total</p>
           <span className="text-span">/ person</span>
         </div>
-        <span className="number-span">$32.79</span>
+        <span className="number-span">${totalAmountPerPerson}</span>
       </div>
-      <button className="cta reset-btn">RESET</button>
+      <button
+        onClick={handleReset}
+        className="cta reset-btn">
+        RESET
+      </button>
     </>
   );
 }
+
 export default Results;
